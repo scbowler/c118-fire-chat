89bb5f8035de6ccf8ccb583427a4cbf0256f0abf
Scott Bowler <scott.bowler@learningfuze.com>
2018-04-25 11:36:50 -0700

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../assets/css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Fire Chat</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
